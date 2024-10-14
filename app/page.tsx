4dc1f1a06bde642c78f83817bc2b436d2b2674c9
Olivli <olivli148@gmail.com>
2024-10-13 20:59:29 -0700

    form

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,31 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart"
+ 
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+ 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
+
 
 //export const description = "A linear area chart"
 
@@ -42,6 +67,19 @@ export default function Home() {
   const [buttons, setButtons] = useState<number[]>([]);
   const containerRef = React.useRef<HTMLDivElement | null>(null);
   
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
 
   const incrementTotalClicks = () => {
     setTotalClicks(totalClicks + 1); // Increment total click count
@@ -181,9 +219,32 @@ export default function Home() {
             </div>
           </CardFooter>
         </Card>
-          
+        
 
       </div>
+
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+    </Form>
       
     </div>
     
